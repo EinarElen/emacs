a53f7e79051163b68aa3b276148fbb6792f20de2
Einar Elen <einar.elen@gmail.com>
2016-09-27 17:47:55 +0200

    stuff

--- init.el
+++ init.el
@@ -13,7 +13,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" default)))
+ '(font-use-system-font t)
  '(org-agenda-files
    (quote
     ("~/ownCloud/org/cal/main.org" "~/.emacs.d/org/refile.org" "~/.emacs.d/configuration.org")))
