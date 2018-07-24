57190b085d183c1e3499404df892b40fc2d4a001
Einar Elen <einar.elen@gmail.com>
2018-07-24 16:24:08 +0200

    P

--- tangle.emacs.el
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/emacs --script
-;; -*- coding: utf-8 -*-
-
-(require 'ox)
-(org-babel-tangle-file "configuration.org")
--- tangle_and_compile.emacs.el
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/emacs --script
-(when (file-newer-than-file-p "~/.emacs.d/configuration.org"
-                              "~/.emacs.d/configuration.el")
-  )
-(load-file "./tangle.emacs.el")
-(load-file "./compile.emacs.el")
