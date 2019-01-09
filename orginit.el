cea25b2a680939145b3d0e99317a0ca61d71b53a
Einar Elén <einar.elen@gmail.com>
2019-01-09 09:20:25 +0100

    EXWM Support

--- orginit.el
+++ orginit.el
@@ -1,14 +1,34 @@
 (require 'package)
 (require 'cl)
-(setq package-enable-at-startup nil)
+(setq gc-cons-threshold 64000000)
+(setq user-emacs-directory
+      (file-name-as-directory
+       (expand-file-name (concat
+                          "~/.emacs.d/"
+                          "machine-local-files"))))
+;(setq package-enable-at-startup nil)
+
+(setq package-user-dir "~/.emacs.d/machine-local-files/elpa/")
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+
 (package-initialize)
 
-(require 'org)
-(require 'ox)
-;; '(require 'ox-twbs)
-(require 'ox-latex)
-(require 'ox-html)
-(require 'ox-odt)
-;; (require 'ox-reveal)
-(require 'ox)
-(setq org-export-async-debug nil)
+(require 'use-package)
+
+
+
+
+(setq use-package-debug t)
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+(setq org-export-async-debug t)
+(use-package org)
+(use-package ox)
+(use-package ox-twbs)
+(use-package ox-latex)
+(use-package ox-html)
+(use-package ox-odt)
+(use-package ox-reveal)
